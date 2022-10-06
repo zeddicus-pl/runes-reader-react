88f787e1123eca146789145812dcfb4510200597
Zeddicus <kamil.tunkiewicz@gmail.com>
2022-10-07 00:04:37 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SaveReader from './SaveReader';
 
 function App() {
+
+  const myButton = () => (
+    <button
+      style={{
+        fontSize: 20,
+      }}
+      onClick={() => {
+        console.log('underneath button was pressed')
+      }}
+    >
+      Read all your save files at once
+    </button>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 30 }}>
+      <p>This is a small React components that finds all of the unsocketed runes in your save files and outputs it into console.</p>
+      <SaveReader
+        element={myButton()}
+        onSubmit={(data) => console.log(data)}
+      />
     </div>
   );
 }
